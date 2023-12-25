eb7dedfcd6454ace69c05b62b45f045094d9121f
GMKrishna99 <gmohankrishna23@gmail.com>
2023-12-25 13:40:16 +0530

    chore : added logo

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
+import SiteLOGO from "@/public/assets/images/logo.svg";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src={SiteLOGO} alt="logo" width={128} height={38} />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
