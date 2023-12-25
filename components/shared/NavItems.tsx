dc08d9f7db1f4da2c34a130c119dd75668341879
GMKrishna99 <gmohankrishna23@gmail.com>
2023-12-25 15:03:10 +0530

    chore : navItems with routes

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,19 @@
+import { headerLinks } from "@/constants";
+import Link from "next/link";
 import React from "react";
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        return (
+          <li className="md:flex-col hover:text-gray-500 text-gray-900 transition-all">
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavItems;
