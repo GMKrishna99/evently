07a8f9069f6bb17ab971921d361e70b97689180e
GMKrishna99 <gmohankrishna23@gmail.com>
2023-12-25 14:51:23 +0530

    chore : added mobile nav and navItems

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,6 +4,8 @@ import SiteLOGO from "@/public/assets/images/logo.svg";
 import Image from "next/image";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -12,9 +14,15 @@ const Header = () => {
         <Link href="/" className="w-36">
           <Image src={SiteLOGO} alt="logo" width={128} height={38} />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
