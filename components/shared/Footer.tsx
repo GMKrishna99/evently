a00f65bcd29a2a093602be0eaee61782600452ac
GMKrishna99 <gmohankrishna23@gmail.com>
2023-12-25 15:14:28 +0530

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Logo from "@/public/assets/images/logo.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t ">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src={Logo} alt="logo" width={128} height={38} />
+        </Link>
+        <p className="text-gray-400 text-sm">
+          2023 Evently. | All Rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
