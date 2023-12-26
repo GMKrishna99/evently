21ab81473cc0bafcae7fb48e609cdda7ad4ca319
GMKrishna99 <gmohankrishna23@gmail.com>
2023-12-26 11:27:06 +0530

    chore : event model schema

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose";
+
+const EventSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, red: "User" },
+});
