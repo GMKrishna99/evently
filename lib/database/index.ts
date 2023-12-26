ecadb530b4750a52dbb3828d2de9673f55dd6d91
GMKrishna99 <gmohankrishna23@gmail.com>
2023-12-26 11:00:41 +0530

    chore : give errors

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,8 +2,24 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+// initialize cached variable
 let cached = (global as any).mongoose || { conn: null, promise: null };
-
+//exporting database
 export const connectToDatabase = async () => {
+  // checking if cached is connected
   if (cached.conn) return cached.conn;
+
+  // Don't have mongodb URI
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
+  // if we have cached connection
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
+    });
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
